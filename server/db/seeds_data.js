6c0975358bc656362dc427408753c1e5057a9e26
Sean Johnson <98856218+sjohns2020@users.noreply.github.com>
2023-08-24 16:14:20 +0100

    feature branch initial commit

--- server/db/seeds_data.js
+++ server/db/seeds_data.js
@@ -2,25 +2,46 @@ const Flight = require("../models/flights.model.js");
 const db = require("../db/db.js");
 
 // Flight objects to seed database
-const flightsData = [
-  {
-    flightNo: "FL001",
-    departure: "GLA",
-    destination: "LDN",
-    date: "2023-08-20"
+const flightsData = [ 
+  { 
+     "FlightNo":"EI3672",
+     "Date":"03\/01\/2017",
+     "Time":"12:25",
+     "ArrDep":"A",
+     "PortOfCallA":"SHANNON",
+     "Status":"LANDED 1232",
+     "OtherInfo":"NOW ON STAND",
+     "Additional":"Baggage at carousel 1",
+     "Airline":"Aer Lingus (Stobart Air)",
+     "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/EI.jpg",
+     "ArrHall":"International"
   },
-  {
-    flightNo: "FL002",
-    departure: "LDN",
-    destination: "DUB",
-    date: "2023-08-21"
+  { 
+     "FlightNo":"BA8926",
+     "Date":"03\/01\/2017",
+     "Time":"12:25",
+     "ArrDep":"A",
+     "PortOfCallA":"SHANNON",
+     "Status":"LANDED 1232",
+     "OtherInfo":"NOW ON STAND",
+     "Additional":"Baggage at carousel 1",
+     "Airline":"British Airways",
+     "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/BA.jpg",
+     "ArrHall":"International"
   },
-  {
-    flightNo: "FL003",
-    departure: "DUB",
-    destination: "EDI",
-    date: "2023-08-22"
-  }
+  { 
+    "FlightNo":"AF1686",
+    "Date":"03\/01\/2017",
+    "Time":"12:30",
+    "ArrDep":"A",
+    "PortOfCallA":"PARIS C D G",
+    "Status":"LANDED 1323",
+    "OtherInfo":"NOW ON STAND",
+    "Additional":"Baggage at carousel 2",
+    "Airline":"Air France",
+    "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/AF.jpg",
+    "ArrHall":"International"
+ }
 ];
 
 
@@ -33,10 +54,107 @@ db.query("DROP TABLE IF EXISTS flights", (err, result) => {
     db.query(
       `CREATE TABLE flights (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        flightNo VARCHAR(255) NOT NULL,
-        departure VARCHAR(255) NOT NULL,
-        destination VARCHAR(255) NOT NULL,
-        date DATE NOT NULL
+        FlightNo VARCHAR(255) NOT NULL,
+        Date DATE NOT NULL,
+        Time TIME NOT NULL,
+        ArrDep CHAR NOT NULL,
+        PortOfCallA VARCHAR(255) NOT NULL,
+        Status VARCHAR(255) NOT NULL,
+        OtherInfo TEXT(1000),
+        Additional TEXT(1000),
+        Airline VARCHAR(255) NOT NULL,
+        Image VARCHAR(255),
+        ArrHall VARCHAR(255) NOT NULL,
+      )`,
+      (err, result) => {
+        if (err) throw err;
+        console.log("Created flights table.");
+  
+      // Insert flight objects into the flights table
+      flightsData.forEach(flight => {
+        Flight.create(new Flight(flight), (err, res) => {
+          if (err) throw err;
+        //   console.log("Created flight:", res);
+        });
+      });
+
+      // Close the database connection (Stops this file hanging)
+      db.end(err => {
+        if (err) throw err;
+        console.log("Database connection closed.");
+      });
+    }
+  );
+});
+
+
+const Flight = require("../models/flights.model.js");
+const db = require("../db/db.js");
+
+// Flight objects to seed database
+const flightsData = [ 
+  { 
+     "FlightNo":"EI3672",
+     "Date":"03\/01\/2017",
+     "Time":"12:25",
+     "ArrDep":"A",
+     "PortOfCallA":"SHANNON",
+     "Status":"LANDED 1232",
+     "OtherInfo":"NOW ON STAND",
+     "Additional":"Baggage at carousel 1",
+     "Airline":"Aer Lingus (Stobart Air)",
+     "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/EI.jpg",
+     "ArrHall":"International"
+  },
+  { 
+     "FlightNo":"BA8926",
+     "Date":"03\/01\/2017",
+     "Time":"12:25",
+     "ArrDep":"A",
+     "PortOfCallA":"SHANNON",
+     "Status":"LANDED 1232",
+     "OtherInfo":"NOW ON STAND",
+     "Additional":"Baggage at carousel 1",
+     "Airline":"British Airways",
+     "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/BA.jpg",
+     "ArrHall":"International"
+  },
+  { 
+    "FlightNo":"AF1686",
+    "Date":"03\/01\/2017",
+    "Time":"12:30",
+    "ArrDep":"A",
+    "PortOfCallA":"PARIS C D G",
+    "Status":"LANDED 1323",
+    "OtherInfo":"NOW ON STAND",
+    "Additional":"Baggage at carousel 2",
+    "Airline":"Air France",
+    "Image":"https:\/\/s3-eu-west-1.amazonaws.com\/ediassets\/img\/airlines\/AF.jpg",
+    "ArrHall":"International"
+ }
+];
+
+
+// Drop the flights table if it already exists exists (Stops duplication when seeds file is run)
+db.query("DROP TABLE IF EXISTS flights", (err, result) => {
+    if (err) throw err;
+    console.log("Dropped existing flights table.");
+  
+    // Create the flights table
+    db.query(
+      `CREATE TABLE flights (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        FlightNo VARCHAR(255) NOT NULL,
+        Date DATE NOT NULL,
+        Time TIME NOT NULL,
+        ArrDep CHAR NOT NULL,
+        PortOfCallA VARCHAR(255) NOT NULL,
+        Status VARCHAR(255) NOT NULL,
+        OtherInfo TEXT(1000),
+        Additional TEXT(1000),
+        Airline VARCHAR(255) NOT NULL,
+        Image VARCHAR(255),
+        ArrHall VARCHAR(255) NOT NULL,
       )`,
       (err, result) => {
         if (err) throw err;
